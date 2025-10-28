5a43708d6608b818933c6f9e03fc3fdad6bf03dd
Guilherme Santos <guilherme.santos.sa@outlook.com>
2025-10-27 23:25:54 -0300

    Home concluida

---
+++ OngHopelink/js/index.js
@@ -0,0 +1,12 @@
+const header = document.querySelector("header")
+const menuBtn = document.querySelector("#menu-btn")
+const closeMenuBtn = document.querySelector("#close-menu-btn")
+
+
+menuBtn.addEventListener("click", () => {
+    header.classList.toggle("mobile-menu");
+});
+
+closeMenuBtn.addEventListener("click", () => {
+   menuBtn.click();
+});
